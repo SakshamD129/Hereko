782488cacd90157015dd9d4e11a656162ab11f7e
SD119 <sakshamsd3@gmail.com>
2025-03-24 12:51:25 +0545

    cmt

--- src/app/@navbar/default.tsx
+++ src/app/@navbar/default.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Image from 'next/image';
-import React from 'react'
 import { useRouter } from 'next/navigation'
 import useCart from '../cart/cart-data';
 function Default() {
@@ -9,7 +8,6 @@ function Default() {
     return (
         <div className='navbar'>
             <Image className='rocket' src="/astro.png" alt='photo' width={40} height={40} onClick={() => router.push("/")} priority></Image>
-
             <button className="navbar-button" onClick={() => router.push('/just')}>About</button>
             <button className="navbar-button" onClick={() => router.push("/reality")}>List</button>
             <div className="cart-img" onClick={() => router.push("/cart-items")}><label>{count}</label><Image src="./cartbelow.svg" alt='Cart:' width={20} height={50}></Image></div>
