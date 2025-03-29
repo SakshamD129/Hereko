41257f0eada1d2bdeef197d59b53182883ffb36d
SD119 <sakshamsd3@gmail.com>
2025-03-29 11:16:46 +0545

    cat

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,9 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
-    // return NextResponse.redirect(new URL("/", request.url));
-    /*const response = NextResponse.next();
-    const themePreference = request.cookies.get("theme");
-    if (!themePreference) {
-        response.cookies.set("theme", "dark");
-    }
-    return response;||*/
     const check = request.nextUrl.pathname;
     if (check === "/biralo") {
         return NextResponse.redirect(new URL("/just", request.nextUrl));
 
     }
 }
-// export const config = {
-// matcher: "/profile",
-// };
